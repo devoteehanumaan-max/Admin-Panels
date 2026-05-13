9b811e0584b4437e90893688ae49fb485249efc0
devoteehanumaan-max <devoteehanumaan@gmail.com>
2026-05-13 21:50:53 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11001691
+    "zoneId": 11001916
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
